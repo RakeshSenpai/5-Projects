7728fa554a8d5ebb8a899c0b6162449cbc3b8da2
Rakesh07778777 <bossrakesh43@gmail.com>
2026-04-09 23:43:03 +0530

    input

--- Project-2/gemini.js
+++ Project-2/gemini.js
@@ -2,11 +2,20 @@ const promptForm = document.querySelector('.prompt-form')
 const promptInput = promptForm.querySelector('.prompt-input')
 
 let userMessage = '';
+
+const createMsgElem = (content, className) => {
+    const div = document.createElement('div')
+    div.classList.add('message' , className)
+    div.innerHTML = content
+    return div;
+}
+
 const handleFromSubmit = (e) => {
     e.preventDefault()
     userMessage = promptInput.value.trim()
     if(!userMessage) return
-    console.log(userMessage)
+    const userMsgHtml = `<p class="message-text"></p>`
+    const userMsgDiv = createMsgElem(userMsgHtml, "user-message")
 }
 
 promptForm.addEventListener('submit' , handleFromSubmit)
