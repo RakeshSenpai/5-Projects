c52600b866c8997668645961f43fedd3e579e6b9
Rakesh07778777 <bossrakesh43@gmail.com>
2026-04-14 20:48:10 +0530

    api response

--- Project-2/gemini.js
+++ Project-2/gemini.js
@@ -1,9 +1,11 @@
 const promptForm = document.querySelector('.prompt-form');
 const promptInput = promptForm.querySelector('.prompt-input');
 const chatContainer = document.querySelector(".chat-container");
+const API_KEY = 'AIzaSyCk768keHxbOP7E2TZ9a-zeRw1lJ47BBGI'
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${API_KEY}`
 
 let userMessage = '';
-
+const chatHistory = []; 
 const createMsgElem = (content, ...classes) => {
     const div = document.createElement('div')
     div.classList.add('message' , ...classes)
@@ -12,10 +14,23 @@ const createMsgElem = (content, ...classes) => {
 }
 
 const generateResponse = async () => {
+    chatHistory.push({
+        role: "user",
+        parts: [{text : userMessage}]
+    })
     try {
-        const respons = await fetch();
+        const respons = await fetch(API_URL , {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify({contents : chatHistory})
+        });
+
+        const data = await respons.json()
+        if(!respons.ok) throw new Error(data.error.message)
+            console.log(data) 
+
     } catch (error) {
-        
+        console.log(error)
     }    
 }
 
